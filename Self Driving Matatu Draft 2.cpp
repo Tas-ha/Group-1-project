4b75fd4595f726a12b293a1c9c0813e5477e6f94
Tas-ha <tasha.wasonga@strathmore.edu>
2026-06-22 19:01:53 +0300

    Update Self Driving Matatu Draft 2.cpp

--- Self Driving Matatu Draft 2.cpp
+++ Self Driving Matatu Draft 2.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+void changeLane(string direction){
+}
+
+void speedUp() {
+}
+
+void brakeMatatu() {
+
+
+void userInput (char input) {
+  switch (input)
+    case 'a':
+        changeLane("left");
+        break;
+    case 'd':
+        changeLane("right");
+        break;
+    case 'w':
+        speedUp();
+        break;
+    case 's':
+        brakeMatatu();
+        break;
+    default:
+        break;
+}
+
 
